f02d0ab5dac14b122196c37f56e54e24dda33a29
Drew Kirkland <drew.kirkland@gmail.com>
2020-06-30 15:51:36 -0400

    Practice/example implemented and intial setup of UserProfile.

--- Gifter/Gifter/Startup.cs
+++ Gifter/Gifter/Startup.cs
@@ -30,7 +30,10 @@ namespace Gifter
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(options =>
+                        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
