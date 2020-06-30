bca351a03442555c5fecec3ed26e8afd4e6bff06
Drew Kirkland <drew.kirkland@gmail.com>
2020-06-30 15:02:25 -0400

    Setup initial structure of Gifter.

---
+++ Gifter/Gifter/Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gifter.Data;
+using Gifter.Repositories;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly PostRepository _postRepository;
+        public PostController(ApplicationDbContext context)
+        {
+            _postRepository = new PostRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_postRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postRepository.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+    }
+}
---
+++ Gifter/Gifter/Repositories/PostRepository.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Gifter.Data;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public class PostRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetAll()
+        {
+            return _context.Post.ToList();
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Post.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
