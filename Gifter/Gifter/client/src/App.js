2e16e35993eabbd394f68b457772b9652422a856
Drew Kirkland <drew.kirkland@gmail.com>
2020-07-02 16:08:35 -0400

    Post form now renders properly.

--- Gifter/Gifter/client/src/App.js
+++ Gifter/Gifter/client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import { PostProvider } from "./providers/PostProvider";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+
 
 function App() {
   return (
     <div className="App">
       <PostProvider>
+        <PostForm />
         <PostList />
       </PostProvider>
     </div>
--- Gifter/Gifter/client/src/components/PostForm.js
+++ Gifter/Gifter/client/src/components/PostForm.js
@@ -13,64 +13,67 @@ import { PostContext } from "../providers/PostProvider";
 const PostForm = (props) => {
   const { addPost } = useContext(PostContext);
 
-    const title = useRef()
-    const caption = useRef()
-    const url = useRef()
+  const title = useRef();
+  const caption = useRef();
+  const url = useRef();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const newPost = {
-        Title: title.current.value,
-        ImageUrl: url.current.value,
-        Caption: caption.current.value,
-        DateCreated: new Date(),
-        UserProfileId: 1,
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addPost(newPost);
-    }
+      Title: title.current.value,
+      ImageUrl: url.current.value,
+      Caption: caption.current.value,
+      DateCreated: new Date(),
+      UserProfileId: 1,
+    };
+    addPost(newPost);
+  };
 
   return (
-    <Form className="form">
-      <Col md={6}>
-        <FormGroup>
-          <Label for="form__title">Title</Label>
-          <Input
-            type="text"
-            name="title"
-            id="form__title"
-            placeholder="Give your GIF a Title"
-            innerRef={title}
-          />
-        </FormGroup>
-      </Col>
-      <Col md={6}>
-        <FormGroup>
-          <Label for="form__url">URL</Label>
-          <Input
-            type="url"
-            name="url"
-            id="form__url"
-            placeholder="www.someurl.com"
-            innerRef={url}
-          />
-        </FormGroup>
-      </Col>
-      <Col md={6}>
-        <FormGroup>
-          <Label for="form__caption">Caption</Label>
-          <Input
-            type="text"
-            name="caption"
-            id="form__caption"
-            placeholder="Caption say what?"
-            innerRef={caption}
-          />
-        </FormGroup>
-      </Col>
-      <Button type="submit" onClick={handleSubmit}>
+    <div className="container">
+      <Form className="form">
+        <Col md={6}>
+          <FormGroup>
+            <Label for="form__title">Title</Label>
+            <Input
+              type="text"
+              name="title"
+              id="form__title"
+              placeholder="Give your GIF a Title"
+              innerRef={title}
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="form__url">URL</Label>
+            <Input
+              type="url"
+              name="url"
+              id="form__url"
+              placeholder="www.someurl.com"
+              innerRef={url}
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="form__caption">Caption</Label>
+            <Input
+              type="text"
+              name="caption"
+              id="form__caption"
+              placeholder="Caption say what?"
+              innerRef={caption}
+            />
+          </FormGroup>
+        </Col>
+        <Button type="submit" onClick={handleSubmit}>
           SUBMIT
-      </Button>
-    </Form>
+        </Button>
+      </Form>
+    </div>
   );
 };
+
+export default PostForm;
